667c0eb3b64800e51764b8aaa68f8dc3ab07b6da
farzanapomy <farzanapomy56@gmail.com>
2021-09-02 05:30:53 +0600

    updated

--- js/style.js
+++ js/style.js
@@ -1,48 +1,53 @@
+const searchField = document.getElementById('search-field');
+const displayContainer = document.getElementById('display-details');
+const searchResul = document.getElementById('total-book')
+
+
 const loadBook = () => {
-    const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
 
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    // data fetch
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayBook(data.docs))
 
-    // console.log(url);
+    // clear field 
     searchField.value = '';
 }
 
 //.slice(0, 10) 
 
 const displayBook = (books) => {
-    const displayContainer = document.getElementById('display-details');
-    console.log(books.length)
-    // const totalBook = document.getElementById('total-book');
-    // totalBook.innerText = `Result found : ${books.length}`;
     displayContainer.textContent = '';
-    books.forEach(book=> {
-    const div = document.createElement('div');
-    div.classList.add('col')
-    div.innerHTML = `
-        <div class="card">
-        <img h-75 src=" https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg">
-            
-            <div class="card-body">
-            <h5 class="card-title"></h5>
-            <p class="card-text">author_name : ${book.author_name}</p>
-            <p class="card-text">Title :${book.subject}</p>
-            <p class="card-text">Publish Date :${book.publish_date[0]}</p>
-            
+    books.forEach(book => {
+        const div = document.createElement('div');
+        div.classList.add('col')
+        div.innerHTML = `
+        <div class="card h-75 ">
+                <img class="h-75" src=" https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg"> 
+           <div class="card-body">
+           
+                 <h5 class="card-title"></h5>
+                 <p class="card-text text-primary fs-2">Book Name : ${book.title}</p>
+                 <p class="card-text text-primary fs-3">author_name : ${book.author_name}</p>
+                <p class="card-text">Publish Date :${book?.publish_date }</p>
+                <p class="card-text">Publisher :${book.publisher.slice(0, 5)}</p>
             </div>
         </div> 
         `
-    displayContainer.appendChild(div);
-    console.log(book);
-})
-const p = document.createElement('p');
-p.innerText = `the result Found: ${data.docs.length}`;
-displayContainer.appendChild(p);
+        displayContainer.appendChild(div);
+        // console.log(book);
+    })
 
-    // books.forEach javascript
+
+    //  error handling for emply field 
+
+    searchResul.innerText = `The result Found: ${books.length}  `;
+
+
+
+
 
 
 }
