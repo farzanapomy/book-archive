f4dbec1da743ede0d9da177e412f42a1e323dacc
farzanapomy <farzanapomy56@gmail.com>
2021-09-02 20:07:12 +0600

    updated

--- js/style.js
+++ js/style.js
@@ -1,17 +1,24 @@
 const searchField = document.getElementById('search-field');
 const displayContainer = document.getElementById('display-details');
 const searchResul = document.getElementById('total-book');
-const ShowBook=document.getElementById('Show-book')
+const showBook = document.getElementById('error')
 
 
 const loadBook = () => {
     const searchText = searchField.value;
-    const url = `https://openlibrary.org/search.json?q=${searchText}`;
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayBook(data));
-    // clear field 
+    // error handle
+    if (searchField.value === '') {
+        showBook.innerText = "No result found.Please enter any book name";
+    }
+    else {
+        error.innerText = ''
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayBook(data));
+    }
+
     searchField.value = '';
 }
 
@@ -23,11 +30,11 @@ const displayBook = books => {
     searchResul.innerText = `The result Found: ${books.numFound}`;
     const bookDetails = books.docs;
     bookDetails.forEach(book => {
-     const div = document.createElement('div');
+        const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = `
         <div class="card h-75  border border-3 border-warning rounded">
-                 <img class="h-75" src=" https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg">
+                 <img class="h-75 w-50" src=" https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg">
            <div class="card-body">
                  <p class="card-title"></p>
                  <p class="card-text text-primary fs-3">Book Name : ${book.title}</p>
@@ -38,10 +45,7 @@ const displayBook = books => {
         </div>
         `
         displayContainer.appendChild(div);
-       
     })
-
-
 }
 
 
